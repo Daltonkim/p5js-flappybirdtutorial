c0703ad4f1db282e63001290a209e6f5d38c1e8e
unknown <you@example.com>
2018-05-21 17:58:10 +0300

    First commit

---
+++ bird.js
@@ -0,0 +1,33 @@
+function Bird(){
+
+	this.y =height/2;
+    this.x = 64;
+    
+    this.gravity = 0.6;
+    this.velocity = 0;
+    this.Lift = 15;
+
+    this.show = function(){
+    	fill(255);
+    	ellipse(this.x, this.y, 32, 32);
+    }
+    this.up =function(){
+     this.velocity += -this.Lift;
+     println(this.velocity);
+    }
+
+    this.update = function()
+    {
+    	this.velocity += this.gravity;
+    	this.y += this.velocity;
+        this.velocity *= 0.9;
+    	if (this.y > height ){
+    		this.y=height;
+    		this.velocity =0;
+    	}
+if (this.y < 0 ){
+    		this.y=0;
+    		this.velocity =0;
+    	}
+    }
+}
